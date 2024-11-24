b5049c8aea30b4f7250376456e9ab5c1256e7902
Allenyzh <allenyangzhenhao@gmail.com>
2024-11-24 13:52:30 -0500

    BugFix: color theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Portfolio from "./pages/Portfolio";
 import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
+import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
 
 import "./App.css";
@@ -16,79 +17,12 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col min-h-screen bg-gray-100 text-slate-950">
-        <div className="navbar bg-base-100 sticky top-0 z-10 text-white">
-          <div className="navbar-start">
-            <div className="dropdown">
-              <div
-                tabIndex={0}
-                role="button"
-                className="btn btn-ghost btn-circle"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h7"
-                  />
-                </svg>
-              </div>
-              <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-              >
-                <li>
-                  <a
-                    onClick={() => setCurrentPage("portfolio")}
-                    className="hover:text-blue-200"
-                  >
-                    Homepage
-                  </a>
-                </li>
-                <li>
-                  <a
-                    onClick={() => {
-                      setCurrentPage("blog");
-                      setCurrentBlogPostId(null);
-                    }}
-                    className="hover:text-blue-200"
-                  >
-                    Blog
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="navbar-center">
-            <a className="btn btn-ghost text-xl">Zhenhao Yang</a>
-          </div>
-          <div className="navbar-end">
-            <button className="btn btn-ghost btn-circle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
-            </button>
-          </div>
-        </div>
-
+      <div className="flex flex-col min-h-screen ">
+   
+        <Navbar
+          setCurrentPage={setCurrentPage}
+          setCurrentBlogPostId={setCurrentBlogPostId}
+        />
         <main className="flex-1">
           {currentPage === "portfolio" && <Portfolio />}
           {currentPage === "blog" &&
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,66 @@
+import ColorThemeSwap from "./ColorThemeSwap";
+
+interface NavbarProps {
+  setCurrentPage: React.Dispatch<React.SetStateAction<"portfolio" | "blog">>;
+  setCurrentBlogPostId: React.Dispatch<React.SetStateAction<number | null>>;
+}
+
+export default function NavBar({
+  setCurrentPage,
+  setCurrentBlogPostId,
+}: NavbarProps) {
+  return (
+    <div className="navbar bg-base-100 sticky top-0 z-10 custom-box-shadow">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a
+                onClick={() => setCurrentPage("portfolio")}
+                className="hover:text-blue-200"
+              >
+                Homepage
+              </a>
+            </li>
+            <li>
+              <a
+                onClick={() => {
+                  setCurrentPage("blog");
+                  setCurrentBlogPostId(null);
+                }}
+                className="hover:text-blue-200"
+              >
+                Blog
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="navbar-center">
+        <a className="btn btn-ghost text-xl">Zhenhao Yang</a>
+      </div>
+      <div className="navbar-end pr-4">
+        <ColorThemeSwap />
+      </div>
+    </div>
+  );
+}
